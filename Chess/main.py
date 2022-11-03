d2acecdc36c5f2937feb5f362a430e61e64293ac
Jakub <jakubr1407@gmail.com>
2022-11-03 18:14:50 +0000

    guide button added

--- Chess/main.py
+++ Chess/main.py
@@ -14,6 +14,8 @@ def draw_text(text, font, text_col, x, y):
 
 #load buttons
 start_img = pygame.image.load('Chess/Sprites/Start-button.png').convert_alpha()
+setting_img = pygame.image.load('Chess/Sprites/Settings Cog.png').convert_alpha()
+guide_img = pygame.image.load('Chess/Sprites/Guide.png').convert_alpha()
 #button class
 class Button():
     def __init__(self, x , y , image , scale):
@@ -26,7 +28,9 @@ class Button():
         #draws button
         screen.blit(self.image,(self.rect.x, self.rect.y))
 
-start_button = Button(200, 100, start_img, 0.5)    
+start_button = Button(280, 200, start_img, 0.5)    
+setting_button = Button(10,10, setting_img, 0.1)
+guide_button = Button(295,325, guide_img, 0.1)
 
 #game_states
 paused = False
@@ -35,7 +39,10 @@ Run = True
 while Run:
 
     screen.fill((55, 72, 33))
+    
     start_button.draw()
+    setting_button.draw()
+    guide_button.draw()
     #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
