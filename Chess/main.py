954c4492191e7a207f3afcf7956834d0c58d7d55
Jakub <jakubr1407@gmail.com>
2022-11-10 12:24:18 +0000

    added some comments for clarity

--- Chess/main.py
+++ Chess/main.py
@@ -1,12 +1,12 @@
+#libraries I will need:
 import pygame
-
 pygame.init()
 
 #creating a display
 screen_width = 600
 screen_height = 600
-# font and text
 
+# font and text
 MenuFont = pygame.font.Font('freesansbold.ttf',32)
 MenuTextX = 200
 MenuTextY = 100
@@ -16,13 +16,11 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Alternative Chess Game")
 
 #draw text onto screen 
-
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x,y))
 
 #load buttons
-
 start_img = pygame.image.load('Chess/Sprites/Start-button.png').convert_alpha()
 setting_img = pygame.image.load('Chess/Sprites/Settings Cog.png').convert_alpha()
 guide_img = pygame.image.load('Chess/Sprites/Guide.png').convert_alpha()
@@ -41,7 +39,7 @@ class Button():
         self.rect.topleft = (x,y)
         self.clicked = False
 
-
+    # special draw function for buttons, includes self.clicked check.
     def draw(self):
         #get mouse position
         pos = pygame.mouse.get_pos()
@@ -57,13 +55,15 @@ class Button():
         
         screen.blit(self.image,(self.rect.x, self.rect.y))
 
+# Creating buttons for the menu screen. 
 start_button = Button(218, 200, start_img, 0.5)    
 setting_button = Button(10,10, setting_img, 0.1)
 guide_button = Button(224,325, guide_img, 0.1)
 
 #game_states
 paused = False
-#game Loop
+
+#Game Loop
 Run = True
 while Run:
     screen.fill((55, 72, 33))
@@ -72,7 +72,7 @@ while Run:
     setting_button.draw()
     guide_button.draw()
     draw_text("Chess Game", MenuFont, textCol, MenuTextX, MenuTextY)
-    #event handler
+    #event handler within main game loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             Run = False
