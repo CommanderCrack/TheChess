0e1824e86b9941a6f0d001f1ca06a53c96e09219
Jakub <jakubr1407@gmail.com>
2023-02-01 14:03:37 +0000

    edited

--- Chess/Engine.py
+++ Chess/Engine.py
@@ -11,3 +11,12 @@ class GameState():
             ["wR","wN","wB","wQ","wK","wB","wN","wR"]]
         self.whiteToMove = True
         self.moveLog = []
+
+class Move():
+    def __init__(self, start_sg, end_sg, board):
+        self.startrow = start_sg[0]
+        self.startcol = start_sg[1]
+        self.endrow = end_sg[0]
+        self.endcol = end_sg[1]
+        self.piecemove = board[self.startrow][self.startcol]
+        self.piececaptured = board[self.endrow][self.endcol]
